f8eef9e97fa03fb5bcaccc025beb2b20d5c967c4
Manuel Martínez <manuel.mtzv816@gmail.com>
2023-05-18 14:16:44 -0600

    fix: arreglar typo en ruta de js en app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resourses/js/app.js')
+  @vite('resources/js/app.js')
   @inertiaHead
 </head>
 
