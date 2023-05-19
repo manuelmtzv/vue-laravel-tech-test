643ca46bc5e36d372ae675a3fd1f64ae178f3042
Manuel Martínez <manuel.mtzv816@gmail.com>
2023-05-18 19:39:10 -0600

    feat: instalar, configurar y probar Tailwindcss
    Este commit realiza los siguientes cambios:
    -. Instala tailwindcss, postcss y autoprefixer.
    -. Crea los archivos de configuración postcss.config.js y tailwind.config.js.
    -. Define las clases de Tailwindcss en css/app.css.
    -. Incluye el resource de css en app.blade.php.
    -. Agrega estilos simples a la lista de hoteles en Pages/Hotels/Index.vue.

--- resources/js/Pages/Hotels/Index.vue
+++ resources/js/Pages/Hotels/Index.vue
@@ -1,9 +1,11 @@
 <template>
     <section>
-        <ul>
+        <ul class="list-disc px-10">
             <li v-for="hotel in hotels">
-                <h1>{{ hotel.name }}</h1>
-                <p>{{ hotel.price }}</p>
+                <article class="flex flex-col gap-4">
+                    <h1 class="text-2xl">{{ hotel.name }}</h1>
+                    <p class="text-lg">{{ hotel.price }}</p>
+                </article>
             </li>
         </ul>
     </section>
