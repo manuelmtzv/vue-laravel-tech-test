547166e8d29a9c57c211af969f5e914ee18b3f34
Manuel Martínez <manuel.mtzv816@gmail.com>
2023-05-18 19:09:12 -0600

    fix: importar librería Inertia en HotelController

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\SaveHotelRequest;
+use Inertia\Inertia;
 use App\Models\Hotel;
+use App\Http\Requests\SaveHotelRequest;
 
 class HotelController extends Controller
 {
