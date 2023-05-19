007ac786d9b2238ad84ade6d4575108abe1e6806
Manuel Martínez <manuel.mtzv816@gmail.com>
2023-05-19 15:06:38 -0600

    feat: crear base del layout del cliente
    Este commit reliza los siguientes cambios:
    -. Crea el archivo Layout.vue dentro de js/Layouts y define una estructura base.
    -. Envuelve el contenido de Hotels/Index.vue dentro del componente Layout.vue.

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mx-auto">
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup></script>
--- resources/js/Pages/Hotels/Index.vue
+++ resources/js/Pages/Hotels/Index.vue
@@ -1,21 +1,31 @@
 <template>
-    <section>
-        <ul class="list-disc px-10">
-            <li v-for="hotel in hotels">
-                <article class="flex flex-col gap-4">
-                    <h1 class="text-2xl">{{ hotel.name }}</h1>
-                    <p class="text-lg">{{ hotel.price }}</p>
-                </article>
-            </li>
-        </ul>
-    </section>
+  <section>
+    <ul class="list-disc">
+      <li v-for="hotel in hotels">
+        <article class="flex flex-col gap-4">
+          <h1 class="text-2xl">{{ hotel.name }}</h1>
+          <p class="text-lg">{{ hotel.price }}</p>
+        </article>
+      </li>
+    </ul>
+  </section>
 </template>
 
+<script>
+import Layout from "../../Layouts/Layout.vue";
+
+export default {
+  layout: Layout,
+};
+</script>
+
 <script setup>
+import Layout from "../../Layouts/Layout.vue";
+
 const props = defineProps({
-    hotels: {
-        type: Array,
-        required: true,
-    },
+  hotels: {
+    type: Array,
+    required: true,
+  },
 });
 </script>
