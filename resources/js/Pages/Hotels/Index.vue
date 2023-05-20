8f9f95563d9950f485651ea98e9cd65133f81739
Manuel Martínez <manuel.mtzv816@gmail.com>
2023-05-20 12:01:30 -0600

    feat: definir estructura y estilos de Hotels/Index
    Este commit realiza los siguientes cambios:
    -. Crea una estructura semántica con HTML para mostrar las entradas de hoteles en Hotels/Index.vue.
    -. Establece nombre de las clases de los elementos siguiendo la convención BEM.
    -. Define los estilos de cada una de las entradas con Grid, estableciendo cantidades de columnas dependiendo de breakpoints, así como transiciones para agregar interacción.
    -. Define que todos las entradas de hoteles son enlaces que utilizan la función route() en su href para mandar a llamar la ruta "hotels.view", dependiendo del id del hotel seleccionado.

--- resources/js/Pages/Hotels/Index.vue
+++ resources/js/Pages/Hotels/Index.vue
@@ -1,13 +1,32 @@
 <template>
-  <section>
-    <ul class="list-disc">
-      <li v-for="hotel in hotels">
-        <article class="flex flex-col gap-4">
-          <h1 class="text-2xl">{{ hotel.name }}</h1>
-          <p class="text-lg">{{ hotel.price }}</p>
-        </article>
-      </li>
-    </ul>
+  <section class="hotels">
+    <a
+      :href="route('hotels.show', hotel)"
+      v-for="hotel in hotels"
+      class="hotels__entry"
+    >
+      <img
+        :src="hotel.image"
+        alt="Hotel ilustrative image"
+        class="hotels__image"
+        loading="lazy"
+      />
+      <div class="hotels__details">
+        <h3 class="hotels__title">{{ hotel.name }}</h3>
+        <p class="hotels__property hotels__property--rating">
+          Rating: <span>{{ hotel.rating }}</span>
+        </p>
+        <p class="hotels__property">
+          City: <span>{{ hotel.city.name }}</span>
+        </p>
+        <p class="hotels__property hotels__property--price">
+          Price:
+          <span>
+            {{ hotel.price }}
+          </span>
+        </p>
+      </div>
+    </a>
   </section>
 </template>
 
@@ -29,3 +48,40 @@ const props = defineProps({
   },
 });
 </script>
+
+<style scoped lang="css">
+.hotels {
+  @apply grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4;
+}
+.hotels__entry {
+  @apply flex flex-col border rounded-lg hover:shadow-md hover:bg-gray-100 transition-all duration-300 cursor-pointer;
+}
+.hotels__image {
+  @apply w-full;
+}
+.hotels__details {
+  @apply flex flex-col gap-2 h-full p-2 justify-between;
+}
+.hotels__title {
+  @apply text-xl font-semibold;
+}
+.hotels__property {
+  @apply text-lg font-semibold;
+}
+.hotels__property--price {
+  @apply text-center;
+}
+.hotels__property--price span {
+  @apply text-green-900;
+}
+.hotels__property--rating span {
+  @apply text-yellow-600;
+}
+.hotels__entry:hover .hotels__property--price span,
+.hotels__entry:hover .hotels__property--rating span {
+  @apply font-semibold;
+}
+.hotels__details span {
+  @apply font-normal;
+}
+</style>
