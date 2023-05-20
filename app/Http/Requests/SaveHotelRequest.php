d6f680ebc1e6b3dea5a82d05caa2e50531be0a32
Manuel Martínez <manuel.mtzv816@gmail.com>
2023-05-20 17:17:56 -0600

    feat: agregar validación regex en rating y price
    Este commit agrega una validación de tipo regex para los campos de "rating" y "price" en el archivo SaveHotelRequest.php:
    -. En el campo "rating" revisa que se tenga una estructura de "{numero} stars".
    -. En "price" se verifica que el precio introducido esté separado por comas cada 3 dígitos.
    
    Esto se ha agregado para verificar la consistencia y calidad de los datos introducidos en el formulario, aún cuando este pueda ser manipulado en el cliente.

--- app/Http/Requests/SaveHotelRequest.php
+++ app/Http/Requests/SaveHotelRequest.php
@@ -23,8 +23,8 @@ class SaveHotelRequest extends FormRequest
   {
     return [
       'name' => ['required', 'string'],
-      'rating' => ['required', 'string'],
-      'price' => ['required', 'string'],
+      'rating' => ['required', 'string', 'regex:/^\d+ stars$/s',],
+      'price' => ['required', 'string', 'regex:/^([1-9]\d{0,2}(,\d{3}){0,2})$|^(([1-9]|1\d|2[1-4])(,\d{3}){3})$|^25(,000){3}$/s'],
       'image' => ['required', 'url'],
       'city_id' => ['required', 'exists:cities,id']
     ];
