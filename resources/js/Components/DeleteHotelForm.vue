c51fbad18f8798a5ba8dcb839bce915682fa00e1
Manuel Martínez <manuel.mtzv816@gmail.com>
2023-05-20 23:41:57 -0600

    feat: crear componente para eliminar un Hotel
    Este commit define un componente con una estructura de form que se encarga de eliminar un registro de la entidad Hotels. Renderiza un botón que elimina el hotel que se le pase.
    Recibe el parmetro "hotel", el cual es un objeto que representa una entrada y esta la envía a la ruta de hotels.delete con la función router.delete() de Inertia y route() de Ziggy para eliminarla.

---
+++ resources/js/Components/DeleteHotelForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <form @submit.prevent="submit" method="POST">
+    <button class="delete" type="submit">Delete</button>
+  </form>
+</template>
+
+<script setup>
+import { router } from "@inertiajs/vue3";
+
+const props = defineProps({
+  hotel: {
+    type: Object,
+    required: true,
+  },
+});
+
+function submit() {
+  router.delete(route("hotels.destroy", props.hotel));
+}
+</script>
+
+<style scoped lang="css">
+.delete {
+  @apply inline-block p-2 text-lg rounded-md border hover:bg-red-400 transition-colors duration-300;
+}
+</style>
